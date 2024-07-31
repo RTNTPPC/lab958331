e06f41e70ea79a1633c6fc8a8f6882006dde0607
RTN <ratana_tar@cmu.ac.th>
2024-07-31 16:03:59 +0700

    Lab7

---
+++ Week7/Express01/app.js
@@ -0,0 +1,38 @@
+/*const express = require('express')
+const app = express()
+
+//console.log("Hello World")
+//console.log("Bye World...")
+
+app.get('/hello', function(req, res){
+    res.send("Hello World <br/>Bye World...")
+    })
+
+app.listen(9999,function(){
+console.log("Server is running on PORT 9999")
+})*/
+
+let http = require('http');
+let url = require('url');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+/*Use the url module to turn the querystring into an object:*/
+var q = url.parse(req.url, true).query;
+/*Return the year and month from the query object:*/
+var txt = q.year + " " + q.month;
+res.end(txt);
+}).listen(9999);
+
+/*
+let http = require('http');
+let url = require('url');
+
+
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+
+var q = url.parse(req.url, true).query;
+
+var txt ="Name :  " + q.name + " <br/>Login Date :"  + q.month + ", " + q.year;
+res.end(txt);
+}).listen(9999);*/
